58d36a8d2ce6c52000b8d5324995f644d3439aee
lacey <hiu@example.com>
2019-01-08 19:49:38 +0000

    pass test 5

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -4,13 +4,25 @@ const Park = function (name, ticketPrice) {
   this.dinosaurs = [];
 };
 
+// create an empty array for dinosaurs as a default -- every time a park is created/instantiated
+// this will set an empty array as the list of dinosaurs.
+
 Park.prototype.addDinosaur = function (dinosaur) {
   this.dinosaurs.push(dinosaur);
 };
 
+// pass in a dinosaur object that is created in the spec file, then push it to the array
 
+Park.prototype.removeDinosaur = function (dinosaurToRemove) {
+  const listOfDinos = this.dinosaurs.filter(dino => dino.species !== dinosaurToRemove.species);
+  this.dinosaurs = listOfDinos
+};
 
-
+// filter will create a copy of an array based on the rules you set in the code block.
+// You need to save it to a variable and call the method upon a property.
+// the dino is a banana, needs the fat arrow -- this code will copy the array into a new array
+// and OMIT (not remove) the object whose .species propery matches the object.species which is
+// passed in.
 
 
 
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -6,10 +6,16 @@ describe('Park', function() {
 
   let park;
   let dinosaur;
+  let dinosaur1;
+  let dinosaur2;
+  let dinosaur3;
 
   beforeEach(function () {
     park = new Park('Onion Park', 10);
     dinosaur = new Dinosaur('t-rex', 'carnivore', 50);
+    dinosaur1 = new Dinosaur('stegosaurus', 'herbivore', 40);
+    dinosaur2 = new Dinosaur('bronto boi', 'herbivore', 30);
+    dinosaur3 = new Dinosaur('aerodactyl', 'omnivore', 100);
   });
 
   it('should have a name', function() {
@@ -33,7 +39,14 @@ describe('Park', function() {
     assert.strictEqual(expected, park.dinosaurs.length)
   });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function() {
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.removeDinosaur(dinosaur2);
+    const expected = [dinosaur1];
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, expected);
+  });
 
   it('should be able to find the dinosaur that attracts the most visitors');
 
